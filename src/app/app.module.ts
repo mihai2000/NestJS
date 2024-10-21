333b5e8e45c6702d6ef35f4a83d7868c4bf54708
mihai2000 <berari.mihaidan@gmail.com>
2024-10-21 13:29:20 +0300

    fixed the configModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,21 @@
+import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
-import { Module } from '@nestjs/common';
-
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from 'src/users/users.module';
 import { PostsModule } from 'src/posts/posts.module';
 import { AuthModule } from 'src/auth/auth.module';
-import appConfig from 'src/config/app.config';
-import databaseConfig from 'src/config/database.config';
-import environmentValidation from 'src/config/environment.validation';
 import { TagsModule } from 'src/tags/tags.module';
 import { MetaOptionsModule } from 'src/meta-options/meta-options.module';
-/**
- * Importing Entities
- * */
+import appConfig from 'src/config/app.config';
+import databaseConfig from 'src/config/database.config';
+import enviromentValidation from '../config/environment.validation';
 
 
-// Get the current NODE_ENV
 const ENV = process.env.NODE_ENV;
-console.log(ENV);
+
 @Module({
   imports: [
     UsersModule,
@@ -31,12 +25,12 @@ console.log(ENV);
       isGlobal: true,
       envFilePath: !ENV ? '.env' : `.env.${ENV}`,
       load: [appConfig, databaseConfig],
-      validationSchema: environmentValidation,
+      validationSchema: enviromentValidation,
     }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
+      useFactory: async (configService: ConfigService) => ({
         type: 'postgres',
         synchronize: configService.get('database.synchronize'),
         port: configService.get('database.port'),
